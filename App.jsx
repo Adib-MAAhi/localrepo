bfd0c1b68f4301626f6c2da25ae9e473aece90f4
Adib-MAAhi <adibmaahi83@gamil.com>
2024-08-20 11:57:58 +0600

    Add initial files

---
+++ App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <form className="form">
+      <div>
+        <h2>Inter you name </h2>
+        <input type="text" placeholder="..name" />
+        <button>Add</button>
+      </div>
+    </form>
+  );
+}
+
+export default App;
