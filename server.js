59e3f33dde54b750ec88d2e6112f18536b3c7e34
MA Ahi <adibmaahi83@gmail.com>
2024-11-09 17:50:54 +0600

    add a new change

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = 5000;
+//const PORT = 5000;
+const PORT = process.env.PORT || 5000; // This lets Render choose the port if it's set
 
 app.use(cors());
 app.use(express.json());
